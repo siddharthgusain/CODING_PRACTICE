b0e061bec3cba1f051b348762a1e4b4f30e8cd20
SIDDHARTH\siddh <siddharthgusain26011999@gmail.com>
2022-07-10 02:28:41 +0530

    lcm and hcf

---
+++ 1.Basics/11.LCM_HCF.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+    int num1, num2;
+    std::cin >> num1 >> num2;
+    int divisor = num2, dividend = num1;
+    while (dividend % divisor != 0)
+    {
+        int remainder = dividend % divisor;
+        dividend = divisor;
+        divisor = remainder;
+    }
+    std::cout << "HCF=" << divisor << std::endl;
+    int hcf = divisor;
+    int lcm = (num1 * num2) / hcf;
+    std::cout << "LCM= " << lcm;
+    return 0;
+}
+
+/*Tip:- LCM * HCF = Num1 *  Num2 */
+
+/*
+Time Comp:- O(min(num1,num2))
+Space Comp:- O(1)
+*/
+
+/*
+Problem:- https://nados.io/question/gcd-and-lcm
+Articles:- https://nados.io/article/gcd-and-lcm
+Videos:- https://www.youtube.com/watch?v=ZtDeDD1VYLk
+*/
