a8a8225a11baa3683cd8cf5aa13b5003439a1030
SIDDHARTH\siddh <siddharthgusain26011999@gmail.com>
2022-07-31 01:36:58 +0530

    arrays intro

---
+++ 0.CPP_LANGUAGE/17.Arrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    int a[10] = {0};              // 10 variables of type integer
+    std::cout << a << std::endl;  // base address
+    std::cout << &a << std::endl; // base address
+    std::cout << *a << std::endl;
+    std::cout << &a[0] << std::endl; // first element address /base address
+    for (int value : a)              // foreach loop -> just a syntactical suger
+    {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+    int *ptr = a;
+    ptr[0] = 1;         // *(ptr + 0)
+    for (int value : a) // foreach loop -> just a syntactical suger
+    {
+        std::cout << value << " ";
+    }
+    return 0;
+}
+
+/*
+-> In assembly , only base address of "a" is used, and as the memory is contigous , next
+array member can be accessed with basic mathematic :-
+base + sizeof(int)* element_number
+-> In assembly , theres nothing known as array
+-> Every member is access with simple mathematic operation
+*/
