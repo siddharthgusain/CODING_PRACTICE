3910711a4114dad010f12eacf6bd202035b3599c
SIDDHARTH\siddh <siddharthgusain26011999@gmail.com>
2022-08-01 23:36:50 +0530

    static array class implementation

--- 0.CPP_LANGUAGE/35.Static_array_class_implementation.cpp
+++ 0.CPP_LANGUAGE/35.Static_array_class_implementation.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
-#include <array>
+
+template <typename T, size_t s>
+class Array
+{
+private:
+    T m_data[s];
+
+public:
+    Array()
+    {
+        for (int i = 0; i < s; i++)
+        {
+            m_data[i] = 0;
+        }
+    }
+
+    size_t size() const
+    {
+        return s;
+    }
+
+    T &operator[](size_t index)
+    {
+        return m_data[index];
+    }
+};
 
 int main()
 {
+    Array<int, 5> arr;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        std::cout << arr[i] << std::endl;
+    }
     return 0;
 }
