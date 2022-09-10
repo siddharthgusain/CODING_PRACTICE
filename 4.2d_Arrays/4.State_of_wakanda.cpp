b2c7fc93549b1ebe35df902f65e4e5a496736431
SIDDHARTH\siddh <siddharthgusain26011999@gmail.com>
2022-09-11 02:07:24 +0530

    state of wakanda

---
+++ 4.2d_Arrays/4.State_of_wakanda.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+    int r1, c1;
+    std::cin >> r1 >> c1;
+    int a[r1][c1] = {0};
+
+    // first matrix
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c1; j++)
+            std::cin >> a[i][j];
+
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c1; j++)
+            std::cout << a[i][j] << " ";
+        std::cout << std::endl;
+    }
+
+    for (int j = 0; j < c1; j++)
+    {
+        if (j % 2 == 0)
+        {
+            for (int i = 0; i < r1; i++)
+                std::cout << a[i][j] << "->";
+        }
+        else
+        {
+            for (int i = r1 - 1; i >= 0; i++)
+                std::cout << a[i][j] << "->";
+        }
+    }
+
+    return 0;
+};
