c9938b9e44fd547bcdc2c5e6d7169f1b5565d7f9
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-10-12 16:17:54 +0530

    some modification to reverse the number

--- 1.1.Basics/8.Reverse_the_original_number.cpp
+++ 1.1.Basics/8.Reverse_the_original_number.cpp
@@ -6,7 +6,8 @@ int reverse_digits(int num)
     int rev_num = 0;
     while (num > 0)
     {
-        rev_num = rev_num * 10 + num % 10;
+        int digit  = num % 10;
+        rev_num = rev_num * 10 + digit;
         num = num / 10;
     }
     return rev_num;
