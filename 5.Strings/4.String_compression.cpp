d51e7f85e0972f5f9f8abb22a7489930e02c68e8
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-10-14 16:37:03 +0530

    resources added

---
+++ 5.Strings/4.String_compression.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+std::string compression1(std::string str)
+{
+    std::string final_string = "";
+    final_string.push_back(str.at(0));
+    
+    return "";
+}
+
+int main()
+{
+    std::string str;
+    std::cin >> str;
+    compression1(str);
+
+    return 0;
+}
