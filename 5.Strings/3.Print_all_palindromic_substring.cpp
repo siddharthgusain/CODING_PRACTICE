bce4e6c3bfe3e3eb17d515ae3a1d5b7bbc9913b6
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-10-14 15:59:16 +0530

    print all palindromic substring

--- 5.Strings/3.Print_all_palindromic_substring.cpp
+++ 5.Strings/3.Print_all_palindromic_substring.cpp
@@ -1,6 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 
-int main(){
-    
+bool is_palindrome(std::string substr)
+{
+    bool is_palindrome = true;
+    for (int i = 0, j = substr.length() - 1; i <= j; i++, j--)
+    {
+        if (substr[i] != substr[j])
+        {
+            is_palindrome = false;
+            break;
+        }
+    }
+    return is_palindrome;
+}
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        for (int j = i + 1; j <= s.length(); j++)
+        {
+            std::string substr = s.substr(i, j - i);
+            if (is_palindrome(substr))
+                std::cout << substr << std::endl;
+        }
+    }
     return 0;
 }
