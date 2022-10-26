03f897c36934078a4283a096242fcef20fd2677b
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-10-26 16:52:49 +0530

    print maze path with jump

---
+++ 6.Recursion_and_backtracking/25.Print_maze_path_with_jump.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void printMazePaths(int sr, int sc, int dr, int dc, string psf)
+{
+    if (sr > dr || sc > dc)
+    {
+        return;
+    }
+
+    if (sr == dr && sc == dc)
+    {
+        cout << psf << endl;
+        return;
+    }
+
+    // horizontal
+    for (int jumps = 1; jumps <= dc - sc; jumps++)
+    {
+        printMazePaths(sr, sc + jumps, dr, dc, psf + "h" + (to_string)(jumps));
+    }
+
+    // vertical
+    for (int jumps = 1; jumps <= dr - sr; jumps++)
+    {
+        printMazePaths(sr + jumps, sc, dr, dc, psf + "v" + (to_string)(jumps));
+    }
+
+    // diagonal
+    for (int jumps = 1; jumps <= dr - sr && jumps <= dc - sc; jumps++)
+    {
+        printMazePaths(sr + jumps, sc + jumps, dr, dc, psf + "d" + (to_string)(jumps));
+    }
+}
+
+int main()
+{
+    int n;
+    int m;
+    cin >> n >> m;
+
+    printMazePaths(0, 0, n - 1, m - 1, "");
+}
+
+/*
+Question :- https://nados.io/question/print-maze-paths-with-jumps
+Article:- https://nados.io/article/print-maze-path-with-jumps
+Video:- https://www.youtube.com/watch?v=zri_tftYphE
+*/
