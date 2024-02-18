984f315a21cdfd4925ed3b489e40d5bb191d59c8
siddharthgusain <siddharthgusain26011999@gmail.com>
2024-02-19 00:07:56 +0530

    Prime Lofic Pdf

--- 1.1.Basics/2.Check_prime.cpp
+++ 1.1.Basics/2.Check_prime.cpp
@@ -2,43 +2,41 @@
 
 void is_prime(int num)
 {
-    if (num <= 1)
+    if (num <= 1) // Negative and 0 Case
     {
-        std::cout << "Neither Prime nor composite";
+        std::cout << "Neither a Prime Nor a Composite Number" << std::endl;
         return;
     }
-    bool flag = false;
+    bool is_prime = false;
     for (int div = 2; div * div <= num; div++)
     {
         if (num % div == 0)
         {
-            flag = true;
+            is_prime = true;
             break;
         }
     }
-    if (flag)
-        std::cout << "Not Prime" << std::endl;
+
+    if (is_prime)
+        std::cout << "Prime" << std::endl;
     else
-        std::cout << "Is Prime" << std::endl;
+        std::cout << "Not Prime" << std::endl;
 }
 
 int main()
 {
     int n;
+    std::cout << "Enter Number of Test Cases:";
     std::cin >> n; // number of test cases
     for (int i = 0; i < n; i++)
     {
+        std::cout << "Enter Number:";
         int num;
         std::cin >> num;
         is_prime(num);
     }
     return 0;
 }
-
-/*
-Time comp:- O(n*sqrt(num))
-Space comp:- O(1)
-*/
 
 /*
 Problem:- https://practice.geeksforgeeks.org/problems/prime-number2314/1
