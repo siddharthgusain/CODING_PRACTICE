86e79340539729fc9737aa076824060cd55da08e
siddharthgusain <siddharthgusain26011999@gmail.com>
2024-06-05 22:12:13 +0530

    90 degree rotate

--- 4.Two_D_Arrays/7.90_degree_rotate.cpp
+++ 4.Two_D_Arrays/7.90_degree_rotate.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 void rotate(int n, vector<vector<int>> &arr)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // Transpose the Matrix
         for (int j = 0; j < i; j++)
             swap(arr[i][j], arr[j][i]);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // Reverse the Matrix
         for (int j = 0; j < n / 2; j++)
             swap(arr[i][j], arr[i][n - 1 - j]);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // Print
     {
         for (int j = 0; j < n; j++)
             cout << arr[i][j] << " ";
