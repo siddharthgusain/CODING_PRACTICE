2410d7b96746f9273f027c26ff6e486005e185b2
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2024-12-19 01:40:03 +0530

    Inbuilt Data types

---
+++ Languages/Java/DataType.java
@@ -0,0 +1,13 @@
+public class DataType {
+    public static void main(String[] args) {
+        // Inbuilt Data types
+        byte a = 10; // 8  bit memory
+        short b = 15000;
+        int c = 100000;
+        long d = 10000000000L;
+        float e = 10.5f;
+        double f = 10.5;
+        char g = 'A';
+        boolean h = true;
+    }
+}
