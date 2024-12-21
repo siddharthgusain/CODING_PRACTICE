4c65d30d51cabb107831725ebf846cdcad71fafb
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2024-12-21 23:19:13 +0530

    Collections

---
+++ 10.Queues/QueueCollection.java
@@ -0,0 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueCollection {
+    public static void main(String[] args) {
+        Queue<Integer> queObj = new ArrayDeque<>();
+        queObj.add(1);
+        queObj.add(2);
+        System.out.println(queObj.poll());
+    }
+}
+
+/*
+ * Here if u see , new ArrayQueue() but assigning to type Queue<Integer>
+ * This is similar to CPP ArrayQueue is Derived Class and we storing its Object
+ * Pointer to queObj which is of Queue Class
+ */
+
+// Queue is Base class -> ArrayDeque is Derived
+// In Java Queue is Interface -> We can't create object of interface as it has
+// ony declaration of function, no definition
--- Languages/Java/Inheritance.java
+++ Languages/Java/Inheritance.java
@@ -1,11 +1,11 @@
 class Parent {
     int count;
 
-    Parent(int count){
+    Parent(int count) {
         System.out.println("Parent Called");
         System.out.println(count);
         this.count = count;
-        
+
     }
 
     protected void show() {
@@ -15,17 +15,17 @@ class Parent {
 
 class Child extends Parent {
 
-    Child(){
+    Child() {
         super(1);
         System.out.println("Child Called");
     }
-    
-    public void show() {  // Can change the visibility to public
+
+    public void show() { // Can change the visibility to public
         System.out.println("Child class method");
     }
 }
 
-public class Inheritance{
+public class Inheritance {
     public static void main(String[] args) {
         Child c = new Child();
         c.show();
