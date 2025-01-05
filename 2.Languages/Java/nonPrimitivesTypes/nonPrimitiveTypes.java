5d32e9c53caeb01a7e3cd080b4c0b1a74359aa1f
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2025-01-05 18:10:14 +0530

    Basic Java

--- 2.Languages/Java/nonPrimitivesTypes/nonPrimitiveTypes.java
+++ 2.Languages/Java/nonPrimitivesTypes/nonPrimitiveTypes.java
@@ -2,7 +2,7 @@ package nonPrimitivesTypes;
 
 public class nonPrimitiveTypes {
     public static void main() {
-        String st = "Hello World";
+        String st = "Hello World"; // Immutable Strings
         System.out.println(st);
 
         int[] arr = { 1, 2, 3, 4, 5 }; // 1D array
