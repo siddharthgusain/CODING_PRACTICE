db2012b8b2d4169dbc4bfbeb83b6b12b80085c7e
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2025-01-06 01:12:02 +0530

    Object class

---
+++ 2.Languages/Java/objectClass/ObjectClass.java
@@ -0,0 +1,9 @@
+public class ObjectClass {
+    public static void main(String[] args) {
+        Object obj = new Object();
+        obj.getClass();
+        obj.toString();
+        obj.hashCode();
+        obj.equals(obj);
+    }
+}
