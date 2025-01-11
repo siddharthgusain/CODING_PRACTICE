f0f3330611733ea07a6425e4e2a600a524b1a24e
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2025-01-11 22:43:27 +0530

    FIle Handling

---
+++ 2.Languages/Java/files/FileHandling.java
@@ -0,0 +1,34 @@
+package files;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileHandling {
+    public static void main(String[] args) {
+
+        try{
+            File meta = new File("name.txt"); // File meta and attributes
+            meta.createNewFile();
+
+            FileWriter f1 =new FileWriter(meta.getName(),true); // For writing to File 
+            f1.write('a');
+            f1.close();
+
+            FileReader f =new FileReader(meta.getName()); // Reading from File
+            while(f.ready()){
+                int data = f.read();
+                System.out.print((char)data);
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
+
+
+/*
+ * Data is of two types 
+ * Internal Program Data -> Inbuilt Data types/Variables , Objects/Custom Data Type
+ * Data from external Device like HDD , NIC , Keyboard -> Known as Files
+ */
