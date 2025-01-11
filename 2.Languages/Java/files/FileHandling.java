6ba0d7e3a4e0a9901fc63ce45c3df976f3ca6b60
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2025-01-11 23:41:50 +0530

    File Handling and Lamda Expression

--- 2.Languages/Java/files/FileHandling.java
+++ 2.Languages/Java/files/FileHandling.java
@@ -19,6 +19,8 @@ public class FileHandling {
                 int data = f.read();
                 System.out.print((char)data);
             }
+
+            meta.delete();
         }catch(Exception e){
             System.out.println(e);
         }
