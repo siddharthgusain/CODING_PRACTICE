0f19e5d3727b24a819e2f64a9b13cb844c504e4f
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2025-01-18 18:42:57 +0530

    Bit Manipulation

---
+++ BIT_manipulation/Basic.java
@@ -0,0 +1,22 @@
+class Basic {
+    public static void main(String[] args) {
+        int n = 3; // 00000011
+        int k = 3;
+        // Binary of 1 -> 00000001
+        int bitOnMask = 1 << k; // 00000100
+        int bitOffMask = ~(1 << k); // 11111011
+        int bitToggleMask = 1 << k; // 00000100
+        int checkBitMask = 1 << k; // 11111011
+
+        System.out.println(n | bitOnMask);
+        System.out.println(n & bitOffMask);
+        System.out.println(n ^ bitToggleMask);
+
+        int result = n & checkBitMask;
+
+        if (result == 0)
+            System.out.println("Bit is Off");
+        else
+            System.out.println("Bit is On");
+    }
+}
