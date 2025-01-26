3db0b8adc2f6f9f7c958b8c4f17d88b9438c82a0
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2025-01-26 23:18:27 +0530

    Reverse LinkedList

---
+++ Blind75/ReverseLinkedList.java
@@ -0,0 +1,59 @@
+package Blind75;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedList {
+
+    private static ListNode reverseListIterative(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    private static ListNode reverseListRecursive(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode newHead = reverseListRecursive(head.next); // Go into Recursion
+
+        // Now Backtracking do something?
+        ListNode front = head.next;
+        front.next  = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        reverseListIterative(null);
+        reverseListRecursive(null);
+    }
+}
