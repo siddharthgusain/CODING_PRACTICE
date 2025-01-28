34cf3f681c8bb1013a5fefab60a957d9f8eeeca1
Siddharth Gusain <32402201+siddharthgusain@users.noreply.github.com>
2025-01-28 23:11:08 +0530

    Merge Two Sorted List

---
+++ Blind75/MergeSortedLinkedLists.java
@@ -0,0 +1,84 @@
+package Blind75;
+
+import java.util.LinkedList;
+
+public class MergeSortedLinkedLists {
+
+    public static LinkedList<Integer> mergeSortedLists(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+        LinkedList<Integer> mergedList = new LinkedList<>();
+        int i = 0, j = 0;
+
+        // Merge elements from both lists while maintaining order
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) <= list2.get(j)) {
+                mergedList.add(list1.get(i));
+                i++;
+            } else {
+                mergedList.add(list2.get(j));
+                j++;
+            }
+        }
+
+        // Add remaining elements from list1
+        while (i < list1.size()) {
+            mergedList.add(list1.get(i));
+            i++;
+        }
+
+        // Add remaining elements from list2
+        while (j < list2.size()) {
+            mergedList.add(list2.get(j));
+            j++;
+        }
+
+        return mergedList;
+        // Time : O(N)
+        // Space: O(N)
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1); // Dummy Node
+        ListNode cur = prehead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = l1 == null ? l2 : l1;
+        return prehead.next;
+        // Time: O(n)
+        // Space : O(1)
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list1 = new LinkedList<>();
+        list1.add(1);
+        list1.add(3);
+        list1.add(5);
+        list1.add(7);
+
+        LinkedList<Integer> list2 = new LinkedList<>();
+        list2.add(2);
+        list2.add(4);
+        list2.add(6);
+        list2.add(8);
+
+        LinkedList<Integer> mergedList = mergeSortedLists(list1, list2);
+        System.out.println("Merged LinkedList: " + mergedList);
+    }
+}
+
+/*
+ * Extreme Naive Solution
+ * 1. Traverse First List -> Add Elements to Array
+ * 2. Traverse Second List -> Add Elements to Array
+ * 3. Sort this Array
+ * 4. Create a new List from this Array
+ */
